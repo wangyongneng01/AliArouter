1128abd8c1dcab849095635017ec0563c3bc4d77
wangyong <wangyongi>
2020-07-15 16:54:45 +0800

    update ignore

--- module_main/src/main/java/com/wanyongi/module_main/MainActivity.java
+++ module_main/src/main/java/com/wanyongi/module_main/MainActivity.java
@@ -1,28 +1,22 @@
 package com.wanyongi.module_main;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.wanyongi.library_base.router.RouterActivityPath;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 
 @Route(path = RouterActivityPath.Main.PAGER_MAIN)
 public class MainActivity extends AppCompatActivity {
 
+    private ViewDataBinding dataBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ((TextView) findViewById(R.id.click)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ARouter.getInstance().build(RouterActivityPath.Message.PAGER_MESSAGE).navigation();
-            }
-        });
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
